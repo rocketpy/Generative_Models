982912870e99e897fcbab356fbe5d0d4b1f5d4db
rocketpy <dimitr.ushakov@gmail.com>
2023-07-31 23:58:50 +0300

    Update Generative_Models_Stability_AI.py

--- Generative_Models_Stability_AI.py
+++ Generative_Models_Stability_AI.py
@@ -1,1 +1,28 @@
+# Generative Models by Stability AI
 
+# https://github.com/Stability-AI/generative-models
+
+# Installation:
+"""
+# 1. Clone the repo
+git clone git@github.com:Stability-AI/generative-models.git
+cd generative-models
+
+2. Setting up the virtualenv
+This is assuming you have navigated to the generative-models root after cloning it.
+
+NOTE: This is tested under python3.8 and python3.10. For other python versions, you might encounter version conflicts.
+
+PyTorch 1.13
+
+# install required packages from pypi
+python3 -m venv .pt13
+source .pt13/bin/activate
+pip3 install -r requirements/pt13.txt
+PyTorch 2.0
+
+# install required packages from pypi
+python3 -m venv .pt2
+source .pt2/bin/activate
+pip3 install -r requirements/pt2.txt
+"""
