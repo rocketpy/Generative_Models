1f36ea0fa7c9aec6edf3887a5dadcbca82bb3f42
rocketpy <dimitr.ushakov@gmail.com>
2023-08-04 23:58:39 +0300

    Update and rename Audi_Craft.py to Audio_Craft.py

---
+++ Audio_Craft.py
@@ -0,0 +1,6 @@
+# AudioCraft is a PyTorch library for deep learning research on audio generation. 
+# AudioCraft contains inference and training code for two state-of-the-art AI generative models producing high-quality audio: AudioGen and MusicGen.
+
+# https://github.com/facebookresearch/audiocraft
+
+
