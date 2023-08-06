fd0180475c955ffc7c661b30c8089532c50160ef
rocketpy <dimitr.ushakov@gmail.com>
2023-08-06 23:53:16 +0300

    Update Audio_Craft.py

--- Audio_Craft.py
+++ Audio_Craft.py
@@ -3,4 +3,17 @@
 
 # https://github.com/facebookresearch/audiocraft
 
+# Installation
+"""
+AudioCraft requires Python 3.9, PyTorch 2.0.0.
+To install AudioCraft, you can run the following:
 
+# Best to make sure you have torch installed first, in particular before installing xformers.
+# Don't run this if you already have PyTorch installed !!!
+pip install 'torch>=2.0'
+
+# Then proceed to one of the following
+pip install -U audiocraft  # stable release
+pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft  # bleeding edge
+pip install -e .  # or if you cloned the repo locally (mandatory if you want to train).
+"""
