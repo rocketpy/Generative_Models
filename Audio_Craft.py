a6c0bb33785fc06d6905cf88def299a028aa26e8
rocketpy <dimitr.ushakov@gmail.com>
2023-08-10 23:56:45 +0300

    Update Audio_Craft.py

--- Audio_Craft.py
+++ Audio_Craft.py
@@ -57,3 +57,11 @@ model.set_generation_params(duration=8)  # generate 8 seconds.
 wav = model.generate_unconditional(4)    # generates 4 unconditional audio samples
 descriptions = ['happy rock', 'energetic EDM', 'sad jazz']
 wav = model.generate(descriptions)  # generates 3 samples.
+
+melody, sr = torchaudio.load('./assets/bach.mp3')
+# generates using the melody from the given audio and the provided descriptions.
+wav = model.generate_with_chroma(descriptions, melody[None].expand(3, -1, -1), sr)
+
+for idx, one_wav in enumerate(wav):
+    # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+    audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
