f654b2cd39d22c71d864d07dd1b8b3a3f45dbe39
rocketpy <dimitr.ushakov@gmail.com>
2023-08-11 23:04:55 +0300

    Create Transformers.py

---
+++ Transformers.py
@@ -0,0 +1,11 @@
+# Transformers - provides thousands of pretrained models to perform tasks on different modalities such as text, vision, and audio.
+
+# https://github.com/huggingface/transformers
+
+# Info:
+These models can be applied on:
+"""
+Text - for tasks like text classification, information extraction, question answering, summarization, translation, text generation, in over 100 languages.
+Images - for tasks like image classification, object detection, and segmentation.
+Audio - for tasks like speech recognition and audio classification.
+"""
