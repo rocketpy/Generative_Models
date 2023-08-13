cbf3c816c47bf0a3cacb7e1936a4f152254ab9de
rocketpy <dimitr.ushakov@gmail.com>
2023-08-13 23:46:14 +0300

    Update Transformers.py

--- Transformers.py
+++ Transformers.py
@@ -3,8 +3,8 @@
 # https://github.com/huggingface/transformers
 
 # Info:
-These models can be applied on:
 """
+These models can be applied on:
 Text - for tasks like text classification, information extraction, question answering, summarization, translation, text generation, in over 100 languages.
 Images - for tasks like image classification, object detection, and segmentation.
 Audio - for tasks like speech recognition and audio classification.
@@ -17,3 +17,14 @@ from transformers import pipeline
 classifier = pipeline('sentiment-analysis')
 classifier('We are very happy to introduce pipeline to the transformers repository.')
 # [{'label': 'POSITIVE', 'score': 0.9996980428695679}]
+
+
+# For example, extract detected objects in an image:
+import requests
+from PIL import Image
+from transformers import pipeline
+
+# Download an image with cute cats
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/coco_sample.png"
+image_data = requests.get(url, stream=True).raw
+image = Image.open(image_data)
