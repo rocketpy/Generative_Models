8deb164335c8a63c5f0b985e6ff8c8a98bdf58c9
rocketpy <dimitr.ushakov@gmail.com>
2023-08-16 23:01:46 +0300

    Update Face_Chain.py

--- Face_Chain.py
+++ Face_Chain.py
@@ -1,3 +1,25 @@
 # FaceChain - FaceChain is a deep-learning toolchain for generating your Digital-Twin.
 
 # https://github.com/modelscope/facechain
+
+
+# Installation
+"""
+Compatibility Verification
+
+The following are the environment dependencies that have been verified:
+
+    python: py3.8, py3.10
+    pytorch: torch2.0.0, torch2.0.1
+    tensorflow: 2.8.0, tensorflow-cpu
+    CUDA: 11.7
+    CUDNN: 8+
+    OS: Ubuntu 20.04, CentOS 7.9
+    GPU: Nvidia-A10 24G
+
+Resource Usage
+
+    GPU: About 19G
+    Disk: About 50GB
+
+"""
