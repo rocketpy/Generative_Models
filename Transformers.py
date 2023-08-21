205b2022f05d2f25bfa41711eeb002714d89aab1
rocketpy <dimitr.ushakov@gmail.com>
2023-08-21 23:46:19 +0300

    Update Transformers.py

--- Transformers.py
+++ Transformers.py
@@ -54,6 +54,13 @@ from transformers import AutoTokenizer, AutoModel
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModel.from_pretrained("bert-base-uncased")
-
 inputs = tokenizer("Hello world!", return_tensors="pt")
+outputs = model(**inputs)
+
+#  for TensorFlow:
+from transformers import AutoTokenizer, TFAutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = TFAutoModel.from_pretrained("bert-base-uncased")
+inputs = tokenizer("Hello world!", return_tensors="tf")
 outputs = model(**inputs)
