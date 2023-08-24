f247f6a31770282e0f7e950f8a52ac40f10aca3c
rocketpy <dimitr.ushakov@gmail.com>
2023-08-24 23:09:59 +0300

    Update Audio_LDM.py

--- Audio_LDM.py
+++ Audio_LDM.py
@@ -1,3 +1,15 @@
 # Audio Generation with AudioLDM
 
 # https://github.com/haoheliu/AudioLDM
+
+# Features:
+"""
+Generate speech, sound effects, music and beyond.
+This repo currently support:
+Text-to-Audio Generation: Generate audio given text input.
+Audio-to-Audio Generation: Given an audio, generate another audio that contain the same type of sound.
+Text-guided Audio-to-Audio Style Transfer: Transfer the sound of an audio into another one using the text description.
+"""
+
+# Commandline Usage
+# https://github.com/haoheliu/AudioLDM#commandline-usage
