e4e94e5d2e69390c9444cd0abca80cd54cb56e1e
rocketpy <dimitr.ushakov@gmail.com>
2023-08-25 23:53:29 +0300

    Update Audio_LDM.py

--- Audio_LDM.py
+++ Audio_LDM.py
@@ -13,3 +13,16 @@ Text-guided Audio-to-Audio Style Transfer: Transfer the sound of an audio into a
 
 # Commandline Usage
 # https://github.com/haoheliu/AudioLDM#commandline-usage
+
+# Web APP
+"""
+The web APP currently only support Text-to-Audio generation. For full functionality please refer to the Commandline Usage.
+
+Prepare running environment
+conda create -n audioldm python=3.8; conda activate audioldm
+pip3 install audioldm
+git clone https://github.com/haoheliu/AudioLDM; cd AudioLDM
+Start the web application (powered by Gradio)
+python3 app.py
+A link will be printed out. Click the link to open the browser and play.
+"""
