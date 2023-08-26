423545d81b7a9b12b1ac0a5cbaf0a132d111e302
rocketpy <dimitr.ushakov@gmail.com>
2023-08-26 23:35:43 +0300

    Create Rif_fusion.py

---
+++ Rif_fusion.py
@@ -0,0 +1,8 @@
+# Riffusion - Riffusion is a library for real-time music and audio generation with stable diffusion.
+
+# https://github.com/riffusion/riffusion
+# https://www.riffusion.com
+
+# Related repositories:
+# Web app: https://github.com/riffusion/riffusion-app
+# Model checkpoint: https://huggingface.co/riffusion/riffusion-model-v1
