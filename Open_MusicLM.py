83f9aef80faab98d1db106c115577f7cd3920f9e
rocketpy <dimitr.ushakov@gmail.com>
2023-08-27 23:04:17 +0300

    Create Open_MusicLM.py

---
+++ Open_MusicLM.py
@@ -0,0 +1,8 @@
+# Open MusicLM - Pytorch implementation of MusicLM
+# Implementation of MusicLM, a text to music model published by Google Research, with a few modifications.
+
+# https://github.com/zhvng/open-musiclm
+
+# Install
+# conda env create -f environment.yaml
+# conda activate open-musiclm
