6ac377fc39189bcd53ad35d0d80b15cbcfca8d34
rocketpy <dimitr.ushakov@gmail.com>
2023-08-27 23:06:17 +0300

    Update Open_MusicLM.py

--- Open_MusicLM.py
+++ Open_MusicLM.py
@@ -6,3 +6,22 @@
 # Install
 # conda env create -f environment.yaml
 # conda activate open-musiclm
+
+# Usage
+# pip install musiclm-pytorch
+
+
+# MuLaN first needs to be trained
+
+import torch
+from musiclm_pytorch import MuLaN, AudioSpectrogramTransformer, TextTransformer
+
+audio_transformer = AudioSpectrogramTransformer(
+    dim = 512,
+    depth = 6,
+    heads = 8,
+    dim_head = 64,
+    spec_n_fft = 128,
+    spec_win_length = 24,
+    spec_aug_stretch_factor = 0.8
+)
