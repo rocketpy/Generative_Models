fd42259f375967a8439a473de70533efa74b27f7
rocketpy <dimitr.ushakov@gmail.com>
2023-08-28 23:49:35 +0300

    Update Open_MusicLM.py

--- Open_MusicLM.py
+++ Open_MusicLM.py
@@ -25,3 +25,29 @@ audio_transformer = AudioSpectrogramTransformer(
     spec_win_length = 24,
     spec_aug_stretch_factor = 0.8
 )
+text_transformer = TextTransformer(
+    dim = 512,
+    depth = 6,
+    heads = 8,
+    dim_head = 64
+)
+
+mulan = MuLaN(
+    audio_transformer = audio_transformer,
+    text_transformer = text_transformer
+)
+
+# get a ton of <sound, text> pairs and train
+
+wavs = torch.randn(2, 1024)
+texts = torch.randint(0, 20000, (2, 256))
+
+loss = mulan(wavs, texts)
+loss.backward()
+
+# after much training, you can embed sounds and text into a joint embedding space
+# for conditioning the audio LM
+
+embeds = mulan.get_audio_latents(wavs)  # during training
+
+embeds = mulan.get_text_latents(texts)  # during inference
