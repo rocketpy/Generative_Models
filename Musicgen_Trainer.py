928dc87f031f7534d3cf2c4277665ac067ed623a
rocketpy <dimitr.ushakov@gmail.com>
2023-08-31 23:58:26 +0300

    Update Musicgen_Trainer.py

--- Musicgen_Trainer.py
+++ Musicgen_Trainer.py
@@ -1,3 +1,10 @@
 # MusicGen Trainer - This is a trainer for MusicGen model.
 
 # https://github.com/chavinlo/musicgen_trainer
+
+
+# To load them, simply run the following on your generation script:
+
+# model.lm.load_state_dict(torch.load('models/lm_final.pt'))
+
+# Where model is the MusicGen Object and models/lm_final.pt is the path to your model (or checkpoint).
