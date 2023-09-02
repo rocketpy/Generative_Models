e2011a528055da2d7890e5166a7eb196d00f016e
rocketpy <dimitr.ushakov@gmail.com>
2023-09-02 23:47:52 +0300

    Update Audiocraft_WebUI.py

--- Audiocraft_WebUI.py
+++ Audiocraft_WebUI.py
@@ -1,1 +1,9 @@
-
+# Audiocraft Infinity WebUI
+"""
+Features:
+Adds generation of songs with a length of over 30 seconds.
+Adds the ability to continue songs.
+Adds a seed option.
+Adds ability to load locally downloaded models.
+"""
+# https://github.com/1aienthusiast/audiocraft-infinity-webui
