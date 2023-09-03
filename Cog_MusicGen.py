2806b83d6a49314d62bb2f5c3120a84363b39c83
rocketpy <dimitr.ushakov@gmail.com>
2023-09-03 23:40:36 +0300

    Create Cog_MusicGen.py

---
+++ Cog_MusicGen.py
@@ -0,0 +1,7 @@
+# Cog implementation of MusicGen
+
+# Cog is an open-source tool that packages machine learning models in a standard, production-ready container.
+# You can deploy your packaged model to your own infrastructure, or to Replicate, where users can interact with it via web interface or API.
+
+# https://github.com/replicate/cog-musicgen
+# https://github.com/replicate/cog
