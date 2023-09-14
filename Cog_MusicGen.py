82c0a93d8345ba90765a3b9a45d26ca46520ce94
rocketpy <dimitr.ushakov@gmail.com>
2023-09-14 23:52:37 +0300

    Update Cog_MusicGen.py

--- Cog_MusicGen.py
+++ Cog_MusicGen.py
@@ -14,6 +14,14 @@
 
 # export REPLICATE_API_TOKEN=<paste-your-token-here>
 
+# Run the model:
+import replicate
+output = replicate.run(
+    "meta/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
+    input={"model_version": "melody"}
+)
+# print(output)
+
 
 #Install
 # For macOS using Homebrew:
