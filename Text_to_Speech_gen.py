689750da2194a9adfb06ab2031d0d21337223267
rocketpy <dimitr.ushakov@gmail.com>
2023-09-19 23:53:15 +0300

    Update Text_to_Speech_gen.py

--- Text_to_Speech_gen.py
+++ Text_to_Speech_gen.py
@@ -1,3 +1,17 @@
 # TTS is a library for advanced Text-to-Speech generation.
 
 # https://github.com/coqui-ai/TTS
+
+
+# Example
+# Running a multi-speaker and multi-lingual model
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models and choose the first one
+model_name = TTS().list_models()[0]
+# Init TTS
+tts = TTS(model_name).to(device)
