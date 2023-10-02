df0b9ac2c5c4cfe5fbb657b7897f21186e0c2e94
rocketpy <dimitr.ushakov@gmail.com>
2023-10-02 23:30:24 +0300

    Update Colossa_AI.py

--- Colossa_AI.py
+++ Colossa_AI.py
@@ -1,5 +1,15 @@
 # Making large AI models cheaper, faster and more accessible 
 
 # https://github.com/hpcaitech/ColossalAI
-#  www.colossalai.org 
+# www.colossalai.org 
+
+# Requirements:
+""""
+    PyTorch >= 1.11 (PyTorch 2.x in progress)
+    Python >= 3.7
+    CUDA >= 11.0
+    NVIDIA GPU Compute Capability >= 7.0 (V100/RTX20 and higher)
+    Linux OS
+"""
+
 
