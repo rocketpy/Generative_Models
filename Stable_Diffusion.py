90e017efe88166626834b9bd85aaa70418b7e5b6
rocketpy <dimitr.ushakov@gmail.com>
2023-10-19 23:41:10 +0300

    Update Stable_Diffusion.py

--- Stable_Diffusion.py
+++ Stable_Diffusion.py
@@ -1,3 +1,28 @@
 # Stable Diffusion - High-Resolution Image Synthesis with Latent Diffusion Models
 
 # https://github.com/CompVis/stable-diffusion
+
+# Requirements
+"""
+conda env create -f environment.yaml
+conda activate ldm
+
+conda install pytorch torchvision -c pytorch
+pip install transformers==4.19.2 diffusers invisible-watermark
+pip install -e .
+"""
+
+# make sure you're logged in with `huggingface-cli login`
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained(
+	"CompVis/stable-diffusion-v1-4", 
+	use_auth_token=True
+).to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+with autocast("cuda"):
+    image = pipe(prompt)["sample"][0]  
+    
+image.save("astronaut_rides_horse.png")
