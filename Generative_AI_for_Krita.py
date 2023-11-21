537302c9d4d67302a3a92736a881ef604134f727
rocketpy <dimitr.ushakov@gmail.com>
2023-11-21 23:54:03 +0200

    Create Generative_AI_for_Krita.py

---
+++ Generative_AI_for_Krita.py
@@ -0,0 +1,10 @@
+# Generative AI for Krita
+
+# https://github.com/Acly/krita-ai-diffusion
+
+# Technology
+"""
+Image generation: Stable Diffusion - https://github.com/Stability-AI/generative-models
+Diffusion backend: ComfyUI - https://github.com/comfyanonymous/ComfyUI
+Inpainting: ControlNet, IP-Adapter: https://github.com/lllyasviel/ControlNet, https://github.com/tencent-ailab/IP-Adapter
+"""
