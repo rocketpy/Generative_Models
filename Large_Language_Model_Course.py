17a6c27db2763ffb991d0c1f0ec06646a0bf66be
rocketpy <dimitr.ushakov@gmail.com>
2023-12-04 23:41:26 +0200

    Create Large_Language_Model_Course.py

---
+++ Large_Language_Model_Course.py
@@ -0,0 +1,10 @@
+# Large Language Model Course
+
+# https://github.com/mlabonne/llm-course
+
+"""
+The LLM course is divided into three parts:
+LLM Fundamentals covers essential knowledge about mathematics, Python, and neural networks.
+The LLM Scientist focuses on learning how to build the best possible LLMs using the latest techniques
+The LLM Engineer focuses on how to create LLM-based solutions and deploy them.
+"""
