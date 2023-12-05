3e2b6c5a18eb1f824b46af42d713f52ba8e9cbd5
rocketpy <dimitr.ushakov@gmail.com>
2023-12-05 22:01:10 +0200

    Create PyTorch.py

---
+++ PyTorch.py
@@ -0,0 +1,4 @@
+# PyTorch Tutorials
+
+# PyTorch Tutorials: Series of videos for complete beginners to learn about PyTorch.
+# https://www.youtube.com/playlist?list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4
