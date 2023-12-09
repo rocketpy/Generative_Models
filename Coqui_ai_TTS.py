f56e91148937be218751c39c1d8af71401497f99
rocketpy <dimitr.ushakov@gmail.com>
2023-12-09 23:49:04 +0200

    Create Coqui_ai_TTS.py

---
+++ Coqui_ai_TTS.py
@@ -0,0 +1,10 @@
+# TTS is a library for advanced Text-to-Speech generation.
+
+# https://github.com/coqui-ai/TTS
+
+"""
+Features:
+Pretrained models in +1100 languages.
+Tools for training new models and fine-tuning existing models in any language.
+Utilities for dataset analysis and curation.
+"""
