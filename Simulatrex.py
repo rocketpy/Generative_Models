2c74a077162d4447e69ec3e64d2a26ea921eafd7
rocketpy <dimitr.ushakov@gmail.com>
2023-12-11 23:41:07 +0200

    Update Simulatrex.py

--- Simulatrex.py
+++ Simulatrex.py
@@ -1,1 +1,5 @@
+# Simulatrex is an open-source project focused on Generative Agent-Based Modeling (GABM).
 
+# https://github.com/simulatrex/simulatrex
+# https://simulatrex.gitbook.io/simulatrex/overview/about
+# pip install simulatrex
