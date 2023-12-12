44c7b7cd11ca80995ab2995a89cdf2d2afcd254b
rocketpy <dimitr.ushakov@gmail.com>
2023-12-12 23:52:20 +0200

    Create Realtime_TTS.py

---
+++ Realtime_TTS.py
@@ -0,0 +1,6 @@
+# RealtimeTTS is a state-of-the-art text-to-speech (TTS) library designed for real-time applications.
+# It stands out in its ability to convert text streams fast into high-quality auditory output with minimal latency.
+
+# https://github.com/KoljaB/RealtimeTTS
+
+# pip install RealtimeTTS
