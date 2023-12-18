d7ccd507ff770b31284669618e23369c5583f19c
rocketpy <dimitr.ushakov@gmail.com>
2023-12-18 23:24:17 +0200

    Update Weak_to_strong.py

--- Weak_to_strong.py
+++ Weak_to_strong.py
@@ -20,3 +20,12 @@ The script accepts several command-line arguments to customize the training proc
 python train_weak_to_strong.py --batch_size 32 --max_ctx 512 --ds_name "sciq" --loss "logconf" --n_docs 1000
 --n_test_docs 100 --weak_model_size "gpt2-medium" --strong_model_size "gpt2-large" --seed 42
 """
+
+# Plotting
+RESULTS_PATH = "../../your_sweep_results_path"
+
+PLOT_ALL_SEEDS = False
+# Full sweep
+MODELS_TO_PLOT = ["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl", "Qwen/Qwen-1_8B", "Qwen/Qwen-7B", "Qwen/Qwen-14B"]
+# Minimal sweep
+# MODELS_TO_PLOT = ["gpt2", "gpt2-medium", "gpt2-large"]
