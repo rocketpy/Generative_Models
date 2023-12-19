8f51e2928aa114ec5f1c4d3d86dc7465937f9eb1
rocketpy <dimitr.ushakov@gmail.com>
2023-12-19 23:37:08 +0200

    Update Coqui_ai_TTS.py

--- Coqui_ai_TTS.py
+++ Coqui_ai_TTS.py
@@ -8,3 +8,23 @@ Pretrained models in +1100 languages.
 Tools for training new models and fine-tuning existing models in any language.
 Utilities for dataset analysis and curation.
 """
+
+# Python API
+# Running a multi-speaker and multi-lingual model
+import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech list of amplitude values as output
+wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
+# Text to speech to a file
+tts.tts_to_file(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
