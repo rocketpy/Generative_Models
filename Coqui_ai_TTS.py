adabd33dc98bca2eb7050a9fb3d286f52a1cabac
rocketpy <dimitr.ushakov@gmail.com>
2023-12-20 23:59:32 +0200

    Update Coqui_ai_TTS.py

--- Coqui_ai_TTS.py
+++ Coqui_ai_TTS.py
@@ -23,8 +23,10 @@ print(TTS().list_models())
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 # Run TTS
+
 # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-# Text to speech list of amplitude values as output
+
 wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
+
 # Text to speech to a file
 tts.tts_to_file(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
