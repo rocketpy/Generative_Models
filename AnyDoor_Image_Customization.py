7c8017fe8941129fdcc5a56757a18b37c057f618
rocketpy <dimitr.ushakov@gmail.com>
2023-12-22 22:59:15 +0200

    Update AnyDoor_Image_Customization.py

--- AnyDoor_Image_Customization.py
+++ AnyDoor_Image_Customization.py
@@ -1,3 +1,23 @@
 # AnyDoor: Zero-shot Object-level Image Customization
 
 # https://github.com/damo-vilab/AnyDoor
+
+# Installation
+"""
+Install with conda:
+
+conda env create -f environment.yaml
+conda activate anydoor
+
+or pip:
+
+pip install -r requirements.txt
+
+Additionally, for training, you need to install panopticapi, pycocotools, and lvis-api.
+
+pip install git+https://github.com/cocodataset/panopticapi.git
+
+pip install pycocotools -i https://pypi.douban.com/simple
+
+pip install lvis
+"""
