a5320a4cb0dee0317d3d715f3086bdabf32c57ff
rocketpy <dimitr.ushakov@gmail.com>
2023-12-23 23:49:51 +0200

    Update mamba_minimal.py

--- mamba_minimal.py
+++ mamba_minimal.py
@@ -2,3 +2,11 @@
 
 # https://github.com/johnma2006/mamba-minimal
 
+
+from model import Mamba
+from transformers import AutoTokenizer
+
+
+model = Mamba.from_pretrained('state-spaces/mamba-370m')
+tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neox-20b')
+generate(model, tokenizer, 'Mamba is the')
