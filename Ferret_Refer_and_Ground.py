bc0aa9ea234fec84e51d29f9004c8131c9110abb
rocketpy <dimitr.ushakov@gmail.com>
2023-12-26 23:30:32 +0200

    Update Ferret_Refer_and_Ground.py

--- Ferret_Refer_and_Ground.py
+++ Ferret_Refer_and_Ground.py
@@ -21,3 +21,20 @@ Install additional packages for training cases
 pip install ninja
 pip install flash-attn --no-build-isolation
 """
+
+# Launch a controller
+"""
+python -m ferret.serve.controller --host 0.0.0.0 --port 10000
+
+Launch a gradio web server.
+
+python -m ferret.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload --add_region_feature
+
+Launch a model worker
+
+This is the worker that load the ckpt and do the inference on the GPU.
+Each worker is responsible for a single model specified in --model-path.
+
+CUDA_VISIBLE_DEVICES=0 python -m ferret.serve.model_worker --host 0.0.0.0 --controller
+http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path ./checkpoints/FERRET-13B-v0 --add_region_feature
+"""
