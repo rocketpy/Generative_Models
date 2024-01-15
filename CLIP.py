5c7cc90ddf4ca1983860f0b61978e139ab0ba99c
rocketpy <dimitr.ushakov@gmail.com>
2024-01-15 22:08:23 +0200

    Create CLIP.py

---
+++ CLIP.py
@@ -0,0 +1,14 @@
+# CLIP - CLIP (Contrastive Language-Image Pre-Training) is a neural network trained on a variety of (image, text) pairs.
+
+# https://github.com/openai/CLIP/tree/main
+
+
+"""
+Usage
+First, install PyTorch 1.7.1 (or later) and torchvision, as well as small additional dependencies, and then install this repo as a Python package.
+On a CUDA GPU machine, the following will do the trick:
+
+$ conda install --yes -c pytorch pytorch=1.7.1 torchvision cudatoolkit=11.0
+$ pip install ftfy regex tqdm
+$ pip install git+https://github.com/openai/CLIP.git
+"""
