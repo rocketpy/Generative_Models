aaee28adb7301698960155e2c5dd01b671ee9cc7
rocketpy <dimitr.ushakov@gmail.com>
2024-01-16 23:17:38 +0200

    Create Apple_AXLearn.py

---
+++ Apple_AXLearn.py
@@ -0,0 +1,22 @@
+# AXLearn - The AXLearn Library for Deep Learning. This library is under active development and the API is subject to change.
+
+# https://github.com/apple/axlearn
+
+"""
+Installing Dependencies
+First, clone the repo. If you intend to develop AXLearn, please fork the repo first, and then clone the fork.
+
+git clone https://github.com/apple/axlearn
+cd axlearn
+(Optional) We recommend installing within a virtual environment, e.g. with conda:
+
+conda create -n axlearn python=3.9
+conda activate axlearn
+The remaining steps depend on your machine's hardware:
+
+Intel (x86) Machines
+Simply do an editable install with:
+
+# Note: This also installs dependencies required for launching jobs to GCP.
+pip install -e '.[gcp]'
+"""
