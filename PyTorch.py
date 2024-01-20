20f30d23d6c803c705071853361f608c9ede5e03
rocketpy <dimitr.ushakov@gmail.com>
2024-01-20 23:41:58 +0200

    Update PyTorch.py

--- PyTorch.py
+++ PyTorch.py
@@ -1,4 +1,7 @@
 # PyTorch Tutorials
 
+# https://github.com/pytorch/pytorch
+# https://pytorch.org/get-started/locally/
+
 # PyTorch Tutorials: Series of videos for complete beginners to learn about PyTorch.
 # https://www.youtube.com/playlist?list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4
