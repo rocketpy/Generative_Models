00893f188ef9c58e00a7841662b2210879e7e89e
rocketpy <dimitr.ushakov@gmail.com>
2024-01-21 23:26:14 +0200

    Update PyTorch.py

--- PyTorch.py
+++ PyTorch.py
@@ -5,3 +5,27 @@
 
 # PyTorch Tutorials: Series of videos for complete beginners to learn about PyTorch.
 # https://www.youtube.com/playlist?list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4
+
+
+# INSTALLATION
+"""
+Anaconda
+To install PyTorch via Anaconda, use the following conda command:
+
+conda install pytorch torchvision -c pytorch
+pip
+To install PyTorch via pip, use one of the following two commands, depending on your Python version:
+
+# Python 3.x
+pip3 install torch torchvision
+"""
+
+# VERIFICATION
+"""
+To ensure that PyTorch was installed correctly, we can verify the installation by running sample PyTorch code.
+Here we will construct a randomly initialized tensor.
+
+import torch
+x = torch.rand(5, 3)
+print(x)
+"""
