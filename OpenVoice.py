034cd34365a25058948805e03ab1ddb5496be0bd
rocketpy <dimitr.ushakov@gmail.com>
2024-01-25 23:08:04 +0200

    Create OpenVoice.py

---
+++ OpenVoice.py
@@ -0,0 +1,18 @@
+# OpenVoice
+
+# https://github.com/myshell-ai/OpenVoice
+# https://research.myshell.ai/open-voice
+
+# Usage - https://github.com/myshell-ai/OpenVoice/blob/main/docs/USAGE.md
+
+"""
+Linux Install
+
+This section is only for developers and researchers who are familiar with Linux, Python and PyTorch. Clone this repo, and run
+
+conda create -n openvoice python=3.9
+conda activate openvoice
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install -r requirements.txt
+
+"""
