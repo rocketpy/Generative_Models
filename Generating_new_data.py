6464395e3c4583c02451764e172438a3c24dae83
rocketpy <dimitr.ushakov@gmail.com>
2024-01-26 21:52:44 +0200

    Update Generating_new_data.py

--- Generating_new_data.py
+++ Generating_new_data.py
@@ -1,6 +1,5 @@
 # Generating new data with Generative Models
 
-# Imports
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,3 +8,12 @@ from keras.models import Sequential, Model
 from keras.layers import Input, Dense, Reshape, Flatten
 from keras.layers.advanced_activations import LeakyReLU
 
+
+# Load data to train the generative model.
+(x_train, y_train), (_, _) = tf.keras.datasets.mnist.load_data()
+
+# Normalize data
+x_train = x_train / 255.0
+
+# Flatten data
+x_train = x_train.reshape(x_train.shape[0], -1)
