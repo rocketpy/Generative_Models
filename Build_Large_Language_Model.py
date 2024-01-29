5b7c2adae6914153d59ab243337d1ab1207c4797
rocketpy <dimitr.ushakov@gmail.com>
2024-01-29 21:15:09 +0200

    Create Build_Large_Language_Model.py

---
+++ Build_Large_Language_Model.py
@@ -0,0 +1,15 @@
+# Build a Large Language Model (From Scratch)
+
+# https://github.com/rasbt/LLMs-from-scratch
+
+# Link to book: https://www.manning.com/books/build-a-large-language-model-from-scratch?utm_source=raschka&utm_medium=affiliate&utm_campaign=book_raschka_build_12_12_23&a_aid=raschka&a_bid=4c2437a0&chan=mm_github
+
+
+# Introduction to PyTorch
+# https://github.com/rasbt/LLMs-from-scratch/blob/main/appendix-A/03_main-chapter-code/code-part1.ipynb
+
+import torch
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+
