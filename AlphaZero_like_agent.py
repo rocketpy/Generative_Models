4b42690b1708115c76f3ca8604a26f254b6dc0f6
rocketpy <dimitr.ushakov@gmail.com>
2024-01-30 23:14:23 +0200

    Create AlphaZero_like_agent.py

---
+++ AlphaZero_like_agent.py
@@ -0,0 +1,20 @@
+# tinyzero - Easily train AlphaZero-like agents on any environment
+
+# https://github.com/s-casci/tinyzero
+
+"""
+Usage
+Make sure you have Python >= 3.8 intalled. After that, run pip install -r requirements.txt to install the necessary dependencies.
+
+Then, to train an agent on one of the existing environments, run:
+
+python3 tictactoe/two_dim/train.py
+where tictactoe/two_dim is the name of the environment you want to train on.
+
+Inside the train script, you can change parameters such as the number of episodes, the number of simulations and enable wandb logging.
+
+Similarly, to evaluate the trained agent run:
+
+python3 tictactoe/two_dim/eval.py
+"""
+
