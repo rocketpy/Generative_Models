05fc79df50a23caf156d14ad1e8ce2a1d18db4e2
rocketpy <dimitr.ushakov@gmail.com>
2024-02-04 22:41:14 +0200

    Update crew_AI.py

--- crew_AI.py
+++ crew_AI.py
@@ -13,3 +13,13 @@ import os
 from crewai import Agent, Task, Crew, Process
 
 os.environ["OPENAI_API_KEY"] = "YOUR KEY"
+
+# You can choose to use a local model through Ollama for example. See ./docs/how-to/llm-connections.md for more information.
+# from langchain.llms import Ollama
+# ollama_llm = Ollama(model="openhermes")
+
+# Install duckduckgo-search for this example:
+# !pip install -U duckduckgo-search
+
+from langchain_community.tools import DuckDuckGoSearchRun
+search_tool = DuckDuckGoSearchRun()
