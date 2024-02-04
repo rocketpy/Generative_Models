4378f753d92a3a9d396791fc62e7caca8278057b
rocketpy <dimitr.ushakov@gmail.com>
2024-02-04 22:41:45 +0200

    Update crew_AI.py

--- crew_AI.py
+++ crew_AI.py
@@ -23,3 +23,24 @@ os.environ["OPENAI_API_KEY"] = "YOUR KEY"
 
 from langchain_community.tools import DuckDuckGoSearchRun
 search_tool = DuckDuckGoSearchRun()
+
+# Define your agents with roles and goals
+researcher = Agent(
+  role='Senior Research Analyst',
+  goal='Uncover cutting-edge developments in AI and data science',
+  backstory="""You work at a leading tech think tank.
+  Your expertise lies in identifying emerging trends.
+  You have a knack for dissecting complex data and presenting actionable insights.""",
+  verbose=True,
+  allow_delegation=False,
+  tools=[search_tool]
+  # You can pass an optional llm attribute specifying what mode you wanna use.
+  # It can be a local model through Ollama / LM Studio or a remote
+  # model like OpenAI, Mistral, Antrophic or others (https://python.langchain.com/docs/integrations/llms/)
+  #
+  # Examples:
+  # llm=ollama_llm # was defined above in the file
+  # llm=OpenAI(model_name="gpt-3.5", temperature=0.7)
+  # For the OpenAI model you would need to import
+  # from langchain_openai import OpenAI
+)
