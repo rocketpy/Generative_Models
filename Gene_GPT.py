5edf20b0008155736a0a02acef987edd3ce5ff42
rocketpy <dimitr.ushakov@gmail.com>
2024-02-13 22:19:04 +0200

    Update Gene_GPT.py

--- Gene_GPT.py
+++ Gene_GPT.py
@@ -1,1 +1,15 @@
-# GeneGPT
+# GeneGPT - code and data for GeneGPT, a tool-augmented LLM for improved access to biomedical information.
+
+# https://github.com/ncbi/GeneGPT
+
+
+# Requirements
+"""
+The code has been tested with Python 3.9.13. Please first install the required packages by:
+
+pip install -r requirements.txt
+You also need an OpenAI API key to run GeneGPT with Codex. Replace the placeholder with your key in config.py:
+
+$ cat config.py 
+API_KEY = 'YOUR_OPENAI_API_KEY'
+"""
