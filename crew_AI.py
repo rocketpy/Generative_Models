694afe3c3c911c07677afc2f83d23d8726fc9738
rocketpy <dimitr.ushakov@gmail.com>
2024-02-16 23:19:22 +0200

    Update crew_AI.py

--- crew_AI.py
+++ crew_AI.py
@@ -83,3 +83,30 @@ result = crew.kickoff()
 
 print("********")
 print(result)
+
+
+# Examples
+
+# AI Crew for Stock Analysis
+"""
+CrewAI Framework
+CrewAI is designed to facilitate the collaboration of role-playing AI agents. In this example,
+these agents work together to give a complete stock analysis and investment recommendation
+
+Running the Script
+It uses GPT-4 by default so you should have access to that to run it.
+
+Disclaimer: This will use gpt-4 unless you changed it not to, and by doing so it will cost you money.
+
+Configure Environment: Copy ``.env.example` and set up the environment variables for Browseless, Serper, SEC-API and OpenAI
+Install Dependencies: Run poetry install --no-root.
+Execute the Script: Run python main.py and input your idea.
+Details & Explanation
+Running the Script: Execute `python main.py`` and input the company to be analyzed when prompted.
+The script will leverage the CrewAI framework to analyze the company and generate a detailed report.
+Key Components:
+./main.py: Main script file.
+./stock_analysis_tasks.py: Main file with the tasks prompts.
+./stock_analysis_agents.py: Main file with the agents creation.
+./tools: Contains tool classes used by the agents.
+"""
