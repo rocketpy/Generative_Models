527365225bd971edb4d5c9b937b472c0876ba6a1
rocketpy <dimitr.ushakov@gmail.com>
2024-02-23 23:59:51 +0200

    Update Open_AI.py

--- Open_AI.py
+++ Open_AI.py
@@ -13,3 +13,16 @@ response = client.chat.completions.create(model="gpt-3.5-turbo",
                                           messages=[{"role": "user", "content": some_text}],
                                           max_tokens=100)
 # print("Response: \n"+response.choices[0].message.content)
+
+
+# To save data sets
+import json
+import weave
+from weave.weaveflow import Dataset
+
+
+weave.init('test')
+with open('test.json') as f:
+    data = json.load(f)
+    dataset = Dataset(data)
+    weave.publish(dataset, 'data')
