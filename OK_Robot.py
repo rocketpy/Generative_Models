23f142eb059bfd3f32d000f4d5b4e8af4ef26161
rocketpy <dimitr.ushakov@gmail.com>
2024-02-24 23:37:51 +0200

    Create OK_Robot.py

---
+++ OK_Robot.py
@@ -0,0 +1,19 @@
+# OK-Robot -  is a zero-shot modular framework that effectively combines the state-of-art navigation and manipulation models to perform pick and place tasks in real homes. 
+
+# https://github.com/ok-robot/ok-robot
+
+
+# Hardware and software requirements
+"""
+Hardware required:
+
+An iPhone Pro with Lidar sensors
+Hello Robot Stretch with Dex Wrist installed
+A workstation with GPU to run pretrained models
+Software required:
+
+Python 3.9
+Record3D (>1.18.0)
+CloudComapre
+"""
+
