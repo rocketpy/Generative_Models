795935db76e37eda7f1bb54728ea8914878ee966
rocketpy <dimitr.ushakov@gmail.com>
2024-02-28 23:30:36 +0200

    Update Home_Robot.py

--- Home_Robot.py
+++ Home_Robot.py
@@ -2,3 +2,21 @@
 
 # https://github.com/facebookresearch/home-robot
 
+# git clone https://github.com/facebookresearch/home-robot --branch home-robot-ovmm-challenge-2023-v0.1.2
+
+
+# Workstation Instructions
+"""
+To set up your workstation, follow these instructions.
+HomeRobot requires Python 3.9. These instructions assume that your system supports CUDA 11.7 or better for pytorch; earlier versions should be fine,
+but may require some changes to the conda environment.
+
+If on Ubuntu, ensure some basic packages are installed:
+
+sudo apt update
+sudo apt install build-essential zip unzip
+Then clone home-robot locally:
+
+git clone https://github.com/facebookresearch/home-robot.git
+cd ./home-robot
+"""
