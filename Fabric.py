ce512bc400208c89abf1d4b7c4e7574d6a41e223
rocketpy <dimitr.ushakov@gmail.com>
2024-03-14 23:51:24 +0200

    Update Fabric.py

--- Fabric.py
+++ Fabric.py
@@ -1,3 +1,43 @@
 # Fabric -  is an open-source framework for augmenting humans using AI.
 
 # https://github.com/danielmiessler/fabric
+
+
+# Quickstart
+"""
+The most feature-rich way to use Fabric is to use the fabric client, which can be found under /client directory in this repository.
+
+Setting up the fabric commands
+Follow these steps to get all fabric related apps installed and configured.
+
+Navigate to where you want the Fabric project to live on your system in a semi-permanent place on your computer.
+# Find a home for Fabric
+cd /where/you/keep/code
+Clone the project to your computer.
+# Clone Fabric to your computer
+git clone https://github.com/danielmiessler/fabric.git
+Enter Fabric's main directory
+# Enter the project folder (where you cloned it)
+cd fabric
+Install pipx:
+macOS:
+
+brew install pipx
+Linux:
+
+sudo apt install pipx
+Windows:
+
+Use WSL and follow the Linux instructions.
+
+Install fabric
+pipx install .
+Run setup:
+fabric --setup
+Restart your shell to reload everything.
+
+Now you are up and running! You can test by running the help.
+
+# Making sure the paths are set up correctly
+fabric --help
+"""
