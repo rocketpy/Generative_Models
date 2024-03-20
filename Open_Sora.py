00c93a57320244e26334ba9cbbb738176a805473
rocketpy <dimitr.ushakov@gmail.com>
2024-03-20 23:43:13 +0200

    Update Open_Sora.py

--- Open_Sora.py
+++ Open_Sora.py
@@ -28,3 +28,18 @@ git clone https://github.com/hpcaitech/Open-Sora
 cd Open-Sora
 pip install -v .
 """
+
+
+# Training
+"""
+To launch training, first download T5 weights into pretrained_models/t5_ckpts/t5-v1_1-xxl.
+Then run the following commands to launch training on a single node.
+
+# 1 GPU, 16x256x256
+torchrun --nnodes=1 --nproc_per_node=1 scripts/train.py configs/opensora/train/16x256x256.py --data-path YOUR_CSV_PATH
+# 8 GPUs, 64x512x512
+torchrun --nnodes=1 --nproc_per_node=8 scripts/train.py configs/opensora/train/64x512x512.py --data-path YOUR_CSV_PATH --ckpt-path YOUR_PRETRAINED_CKPT
+To launch training on multiple nodes, prepare a hostfile according to ColossalAI, and run the following commands.
+
+colossalai run --nproc_per_node 8 --hostfile hostfile scripts/train.py configs/opensora/train/64x512x512.py --data-path YOUR_CSV_PATH --ckpt-path YOUR_PRETRAINED_CKPT
+"""
