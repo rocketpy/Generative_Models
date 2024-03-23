9f3d3dc0665fef596b0f3de212344a03f0f070c8
rocketpy <dimitr.ushakov@gmail.com>
2024-03-23 23:53:34 +0200

    Create Nerd_Dictation.py

---
+++ Nerd_Dictation.py
@@ -0,0 +1,21 @@
+# Nerd Dictation - Offline Speech to Text for Desktop Linux
+
+# https://github.com/ideasman42/nerd-dictation
+
+# Features
+"""
+Simple
+This is a single file Python script with minimal dependencies.
+Hackable
+User configuration lets you manipulate text using Python string operations.
+Zero Overhead
+As this relies on manual activation there are no background processes.
+Dictation is accessed manually with begin/end commands.
+
+# Usage
+It is suggested to bind begin/end/cancel to shortcut keys.
+
+nerd-dictation begin
+nerd-dictation end
+For details on how this can be used, see: nerd-dictation --help and nerd-dictation begin --help.
+"""
