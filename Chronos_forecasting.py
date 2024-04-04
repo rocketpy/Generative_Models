3921aed8fb80d954d51308533a83590f386af1c2
rocketpy <dimitr.ushakov@gmail.com>
2024-04-04 23:44:34 +0300

    Create Chronos_forecasting.py

---
+++ Chronos_forecasting.py
@@ -0,0 +1,9 @@
+# Chronos: Learning the Language of Time Series
+
+# https://github.com/amazon-science/chronos-forecasting
+
+
+# Usage
+# To perform inference with Chronos models, install this package by running:
+
+# pip install git+https://github.com/amazon-science/chronos-forecasting.git
