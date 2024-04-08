004c3278695f19d8745cc1345d7191427b5019de
rocketpy <dimitr.ushakov@gmail.com>
2024-04-08 23:53:54 +0300

    Update Chronos_forecasting.py

--- Chronos_forecasting.py
+++ Chronos_forecasting.py
@@ -7,3 +7,20 @@
 # To perform inference with Chronos models, install this package by running:
 
 # pip install git+https://github.com/amazon-science/chronos-forecasting.git
+# pip install pandas matplotlib
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from chronos import ChronosPipeline
+
+pipeline = ChronosPipeline.from_pretrained(
+    "amazon/chronos-t5-small",
+    device_map="cuda",  # use "cpu" for CPU inference and "mps" for Apple Silicon
+    torch_dtype=torch.bfloat16,
+)
+
+df = pd.read_csv("https://raw.githubusercontent.com/AileenNielsen/TimeSeriesAnalysisWithPython/master/data/AirPassengers.csv")
+
