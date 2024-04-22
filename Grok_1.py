dd06e17a88c38df389857dac4343064a3a8131de
rocketpy <dimitr.ushakov@gmail.com>
2024-04-22 23:58:50 +0300

    Create Grok_1.py

---
+++ Grok_1.py
@@ -0,0 +1,13 @@
+# Grok-1 - This repository contains JAX example code for loading and running the Grok-1 open-weights model.
+
+# https://github.com/xai-org/grok-1
+
+
+"""
+Then, run
+
+pip install -r requirements.txt
+python run.py
+
+to test the code.
+"""
