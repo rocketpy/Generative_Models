f9de340b44422e0b9719ee781b18483a269dd0fd
rocketpy <dimitr.ushakov@gmail.com>
2024-05-22 23:59:35 +0300

    Update DeepFloyd_IF.py

--- DeepFloyd_IF.py
+++ DeepFloyd_IF.py
@@ -2,3 +2,12 @@
 
 # https://github.com/deep-floyd/IF
 
+
+# pip install deepfloyd_if==1.0.2rc0
+# pip install xformers==0.0.16
+# pip install git+https://github.com/openai/CLIP.git --no-deps
+
+
+from huggingface_hub import login
+
+login()
