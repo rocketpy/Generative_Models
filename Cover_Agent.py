6fa36570062dec07beef952402fd5dce2d737c78
rocketpy <dimitr.ushakov@gmail.com>
2024-05-23 23:41:05 +0300

    Create Cover_Agent.py

---
+++ Cover_Agent.py
@@ -0,0 +1,4 @@
+# Cover-Agent - CodiumAI Cover Agent aims to help efficiently increasing code coverage, by automatically generating qualified tests to enhance existing test suites
+
+# https://github.com/Codium-ai/cover-agent
+
