70c8a84db680642b66d31b8d127b9f900a94b7f4
rocketpy <dimitr.ushakov@gmail.com>
2024-05-24 23:23:42 +0300

    Create Diffusers.py

---
+++ Diffusers.py
@@ -0,0 +1,4 @@
+# Diffusers - is the go-to library for state-of-the-art pretrained diffusion models for generating images, audio, and even 3D structures of molecules. 
+
+# https://github.com/huggingface/diffusers
+
