6c091f40e6d1a82d9c71537c88ca742fe923d685
rocketpy <dimitr.ushakov@gmail.com>
2024-05-26 23:51:04 +0300

    Create AI_Workshop_code.py

---
+++ AI_Workshop_code.py
@@ -0,0 +1,6 @@
+# Web scraping for LLM
+# Long term memory & personalized LLM responses
+
+
+# https://github.com/trancethehuman/ai-workshop-code/blob/main/Web_scraping_for_LLM_in_2024.ipynb
+# https://github.com/trancethehuman/ai-workshop-code/blob/main/Long_term_memory_%26_personalized_LLM_responses.ipynb
