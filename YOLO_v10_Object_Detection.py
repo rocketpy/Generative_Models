3780cb3ab8b45543eddaffa432cf7aeadb6c734e
rocketpy <dimitr.ushakov@gmail.com>
2024-05-28 23:51:28 +0300

    Create YOLO_v10_Object_Detection.py

---
+++ YOLO_v10_Object_Detection.py
@@ -0,0 +1,14 @@
+# YOLOv10: Real-Time End-to-End Object Detection
+
+# https://github.com/THU-MIG/yolov10
+
+
+# Installation
+"""
+conda virtual environment is recommended.
+
+conda create -n yolov10 python=3.9
+conda activate yolov10
+pip install -r requirements.txt
+pip install -e .
+"""
