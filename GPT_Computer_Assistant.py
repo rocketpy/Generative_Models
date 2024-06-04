609c5c7fc5e3c7a9e9c22d085b1bd3535088660c
rocketpy <dimitr.ushakov@gmail.com>
2024-06-04 23:44:07 +0300

    Create GPT_Computer_Assistant.py

---
+++ GPT_Computer_Assistant.py
@@ -0,0 +1,11 @@
+# GPT Computer Assistant - this is an alternative work for providing ChatGPT MacOS app to Windows and Linux. 
+
+# https://github.com/onuratakan/gpt-computer-assistant
+
+"""
+Installation && Run
+Needed >= Python 3.9
+
+pip3 install gpt-computer-assistant
+computerassistant
+"""
