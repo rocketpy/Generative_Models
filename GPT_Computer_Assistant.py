bffab1c9f0ef69ec42196994fbe3c677c8326a06
rocketpy <dimitr.ushakov@gmail.com>
2024-06-12 23:12:55 +0300

    Update GPT_Computer_Assistant.py

--- GPT_Computer_Assistant.py
+++ GPT_Computer_Assistant.py
@@ -9,3 +9,25 @@ Needed >= Python 3.9
 pip3 install gpt-computer-assistant
 computerassistant
 """
+
+# Agent Infrastructure | NEW
+# To create crewai agents and using it into gpt-computer-assistant gui and tools.
+
+# pip3 install gpt-computer-assistant[agentic]
+
+
+from gpt_computer_assistant import Agent, start
+
+manager = Agent(
+  role='Project Manager',
+  goal='understands project needs and assist coder',
+  backstory="""You're a manager at a large company.""",
+)
+
+coder = Agent(
+  role='Senior Python Coder',
+  goal='writing python scripts and copying to clipboard',
+  backstory="""You're a python developer at a large company.""",
+)
+
+start()
