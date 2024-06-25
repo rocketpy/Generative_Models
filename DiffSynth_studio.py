ab13041d5297bd0eaa3dfd573578ced4344440f6
rocketpy <dimitr.ushakov@gmail.com>
2024-06-25 23:14:24 +0300

    Create DiffSynth_studio.py

---
+++ DiffSynth_studio.py
@@ -0,0 +1,11 @@
+# DiffSynth Studio - is a Diffusion engine. 
+
+# https://github.com/modelscope/DiffSynth-Studio
+
+
+# Installation
+"""
+git clone https://github.com/modelscope/DiffSynth-Studio.git
+cd DiffSynth-Studio
+pip install -e .
+"""
