a3d9f8692a7b63769203906875bf5ee1bd429eb3
rocketpy <dimitr.ushakov@gmail.com>
2024-07-01 23:16:04 +0300

    Update Gemma_in_PyTorch.py

--- Gemma_in_PyTorch.py
+++ Gemma_in_PyTorch.py
@@ -3,6 +3,10 @@
 # https://github.com/google/gemma_pytorch
 
 
+# Try it free on Colab
+# https://ai.google.dev/gemma/docs/pytorch_gemma
+
+
 """
 Download Gemma model checkpoint
 
@@ -18,4 +22,14 @@ VARIANT=<2b or 7b or 9b or 27b>
 CKPT_PATH=<Insert ckpt path here>
 Try it free on Colab
 Follow the steps at https://ai.google.dev/gemma/docs/pytorch_gemma.
+
+Try it out with PyTorch
+Prerequisite: make sure you have setup docker permission properly as a non-root user.
+
+sudo usermod -aG docker $USER
+newgrp docker
+Build the docker image.
+DOCKER_URI=gemma:${USER}
+
+docker build -f docker/Dockerfile ./ -t ${DOCKER_URI}
 """
