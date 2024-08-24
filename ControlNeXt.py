3c3b7d9966da5a68ec17c4b9b0400c8f6d162e75
rocketpy <dimitr.ushakov@gmail.com>
2024-08-25 02:01:06 +0300

    Create ControlNeXt.py

---
+++ ControlNeXt.py
@@ -0,0 +1,6 @@
+# ControlNeXt - is implementation for controllable generation, supporting both images and videos while incorporating diverse forms of control information.
+
+# https://github.com/dvlab-research/ControlNeXt
+
+# https://pbihao.github.io/projects/controlnext/index.html
+
