1da15546373147ae07893fe04ba53d680a8701f0
rocketpy <dimitr.ushakov@gmail.com>
2024-10-27 23:58:58 +0200

    Update F5_TTS.py

--- F5_TTS.py
+++ F5_TTS.py
@@ -3,3 +3,11 @@
 
 # https://github.com/SWivid/F5-TTS
 
+
+# Installation
+# Create a python 3.10 conda env (you could also use virtualenv)
+conda create -n f5-tts python=3.10
+conda activate f5-tts
+
+# Install pytorch with your CUDA version, e.g.
+pip install torch==2.3.0+cu118 torchaudio==2.3.0+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
