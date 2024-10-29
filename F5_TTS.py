6131bfe9b2d0b33dfc19adbb4a521a5e5751a857
rocketpy <dimitr.ushakov@gmail.com>
2024-10-29 23:59:52 +0200

    Update F5_TTS.py

--- F5_TTS.py
+++ F5_TTS.py
@@ -11,3 +11,21 @@ conda activate f5-tts
 
 # Install pytorch with your CUDA version, e.g.
 pip install torch==2.3.0+cu118 torchaudio==2.3.0+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+
+
+# As a pip package (if just for inference)
+pip install git+https://github.com/SWivid/F5-TTS.git
+
+
+# Local editable (if also do training, finetuning)
+git clone https://github.com/SWivid/F5-TTS.git
+cd F5-TTS
+pip install -e .
+
+  
+# Docker usage
+# Build from Dockerfile
+docker build -t f5tts:v1 .
+
+# Or pull from GitHub Container Registry
+docker pull ghcr.io/swivid/f5-tts:main
