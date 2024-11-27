a92f6805139edf47147945bce2d536355dbddbe9
rocketpy <dimitr.ushakov@gmail.com>
2024-11-27 23:41:02 +0200

    Update FLUX.py

--- FLUX.py
+++ FLUX.py
@@ -11,3 +11,21 @@ python3.10 -m venv .venv
 source .venv/bin/activate
 pip install -e ".[all]"
 """
+
+
+from flux.api import ImageRequest
+
+
+# this will create an api request directly but not block until the generation is finished
+request = ImageRequest("A beautiful beach", name="flux.1.1-pro")
+# or: request = ImageRequest("A beautiful beach", name="flux.1.1-pro", api_key="your_key_here")
+
+# any of the following will block until the generation is finished
+request.url
+# -> https:<...>/sample.jpg
+request.bytes
+# -> b"..." bytes for the generated image
+request.save("outputs/api.jpg")
+# saves the sample to local storage
+request.image
+# -> a PIL image
