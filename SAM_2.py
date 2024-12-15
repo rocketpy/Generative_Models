d96c44b22b635def2f860317a13746080ba255d7
rocketpy <dimitr.ushakov@gmail.com>
2024-12-15 23:41:16 +0200

    Create SAM_2.py

---
+++ SAM_2.py
@@ -0,0 +1,5 @@
+# SAM 2 - Segment Anything in Images and Videos
+# Segment Anything Model 2 (SAM 2) is a foundation model towards solving promptable visual segmentation in images and videos. 
+
+# https://github.com/facebookresearch/sam2
+
