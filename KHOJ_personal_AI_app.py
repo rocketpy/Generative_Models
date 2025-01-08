2c7e087b72085c12011c8442118eef17fda6b03d
rocketpy <dimitr.ushakov@gmail.com>
2025-01-08 23:36:03 +0200

    Create KHOJ_personal_AI_app.py

---
+++ KHOJ_personal_AI_app.py
@@ -0,0 +1,17 @@
+# KHOJ - is a personal AI app to extend your capabilities. It smoothly scales up from an on-device personal AI to a cloud-scale enterprise AI.
+
+# https://github.com/khoj-ai/khoj
+
+# Features:
+"""
+- Chat with any local or online LLM (e.g llama3, qwen, gemma, mistral, gpt, claude, gemini).
+- Get answers from the internet and your docs (including image, pdf, markdown, org-mode, word, notion files).
+- Access it from your Browser, Obsidian, Emacs, Desktop, Phone or Whatsapp.
+- Create agents with custom knowledge, persona, chat model and tools to take on any role.
+- Automate away repetitive research. Get personal newsletters and smart notifications delivered to your inbox.
+- Find relevant docs quickly and easily using our advanced semantic search.
+- Generate images, talk out loud, play your messages.
+- Khoj is open-source, self-hostable. Always.
+- Run it privately on your computer or try it on our cloud app.
+"""
+
