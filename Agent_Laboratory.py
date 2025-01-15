617d8ec12172e1cf9d038537281d7d25819bab39
rocketpy <dimitr.ushakov@gmail.com>
2025-01-15 23:53:45 +0200

    Update Agent_Laboratory.py

--- Agent_Laboratory.py
+++ Agent_Laboratory.py
@@ -1,3 +1,24 @@
 # Agent Laboratory - Using LLM Agents as Research Assistants
 
 # https://github.com/SamuelSchmidgall/AgentLaboratory
+
+
+# Installation
+"""
+Python venv option
+We recommend using python 3.12
+Clone the GitHub Repository: Begin by cloning the repository using the command:
+git clone git@github.com:SamuelSchmidgall/AgentLaboratory.git
+
+Set up and Activate Python Environment
+python -m venv venv_agent_lab
+
+Now activate this environment:
+source venv_agent_lab/bin/activate
+
+Install required libraries
+pip install -r requirements.txt
+
+Install pdflatex [OPTIONAL]
+sudo apt install pdflatex
+"""
