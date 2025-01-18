1116ba3b6fe00cdd3d948c3d1bdb175f9be9863f
rocketpy <dimitr.ushakov@gmail.com>
2025-01-18 23:49:08 +0200

    Create MiniCPM.py

---
+++ MiniCPM.py
@@ -0,0 +1,10 @@
+# MiniCPM-o - A GPT-4o Level MLLM for Vision, Speech and Multimodal Live Streaming on Your Phone
+
+# https://github.com/OpenBMB/MiniCPM-o
+
+# Features:
+"""
+MiniCPM-o is the latest series of end-side multimodal LLMs (MLLMs) ungraded from MiniCPM-V. 
+The models can now take image, video, text, and audio as inputs and provide high-quality text and speech outputs in an end-to-end fashion. 
+Since February 2024, we have released 6 versions of the model, aiming to achieve strong performance and efficient deployment. 
+"""
