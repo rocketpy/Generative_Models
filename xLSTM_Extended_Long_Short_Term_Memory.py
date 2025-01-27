dfa4f36920dcfbb58c3940d14fee6109c2404c8f
rocketpy <dimitr.ushakov@gmail.com>
2025-01-27 23:44:25 +0200

    Update xLSTM_Extended_Long_Short_Term_Memory.py

--- xLSTM_Extended_Long_Short_Term_Memory.py
+++ xLSTM_Extended_Long_Short_Term_Memory.py
@@ -2,3 +2,29 @@
 
 # https://github.com/NX-AI/xlstm
 
+
+# Features
+"""
+xLSTM is a new Recurrent Neural Network architecture based on ideas of the original LSTM. 
+Through Exponential Gating with appropriate normalization and stabilization techniques and 
+a new Matrix Memory it overcomes the limitations of the original LSTM and shows promising performance 
+on Language Modeling when compared to Transformers or State Space Models.
+"""
+
+# Installation
+"""
+Create a conda environment from the file environment_pt220cu121.yaml. 
+Install the model code only (i.e. the module xlstm) as package:
+
+Install via pip:
+
+pip install xlstm
+Clone from github:
+
+git clone https://github.com/NX-AI/xlstm.git
+cd xlstm
+pip install -e .
+For using the 7B xLSTM model install mlstm_kernels via:
+
+pip install mlstm_kernels
+"""
