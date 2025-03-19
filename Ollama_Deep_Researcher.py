355228c36b5df55ef924201c0ce5e5ffefb647b9
rocketpy <dimitr.ushakov@gmail.com>
2025-03-19 23:37:41 +0200

    Update Ollama_Deep_Researcher.py

--- Ollama_Deep_Researcher.py
+++ Ollama_Deep_Researcher.py
@@ -1,3 +1,25 @@
 # Ollama Deep Researcher - Ollama Deep Researcher is a fully local web research assistant that uses any LLM hosted
 
 # https://github.com/langchain-ai/ollama-deep-researcher
+
+
+# Quickstart
+"""
+Mac
+
+Download the Ollama app for Mac here.
+
+Pull a local LLM from Ollama. As an example:
+
+ollama pull deepseek-r1:8b
+
+Clone the repository:
+git clone https://github.com/langchain-ai/ollama-deep-researcher.git
+cd ollama-deep-researcher
+
+Select a web search tool:
+By default, it will use DuckDuckGo for web search, which does not require an API key. 
+But you can also use Tavily or Perplexity by adding their API keys to the environment file:
+
+cp .env.example .env
+"""
