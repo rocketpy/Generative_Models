d5298bf443ec63725d8dfc66be7a940845949e5e
rocketpy <dimitr.ushakov@gmail.com>
2025-03-21 23:28:27 +0200

    Update Ollama_Deep_Researcher.py

--- Ollama_Deep_Researcher.py
+++ Ollama_Deep_Researcher.py
@@ -22,4 +22,17 @@ By default, it will use DuckDuckGo for web search, which does not require an API
 But you can also use Tavily or Perplexity by adding their API keys to the environment file:
 
 cp .env.example .env
+
+
+# Running with LangGraph Studio
+
+Mac
+(Recommended) Create a virtual environment:
+python -m venv .venv
+source .venv/bin/activate
+
+Launch LangGraph server:
+# Install uv package manager
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uvx --refresh --from "langgraph-cli[inmem]" --with-editable . --python 3.11 langgraph dev
 """
