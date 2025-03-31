9c9b695ef7af8462e498cb85d050358cf990c37a
rocketpy <dimitr.ushakov@gmail.com>
2025-03-31 21:47:17 +0300

    Create AI_Search_Assistant.py

---
+++ AI_Search_Assistant.py
@@ -0,0 +1,11 @@
+# AI Search Assistant - This Streamlit app combines the power of search engines and LLMs to provide you with pinpointed answers to your queries. 
+
+# https://github.com/Shubhamsaboo/awesome-llm-apps/tree/main/advanced_tools_frameworks/web_search_ai_assistant
+
+
+# Features:
+"""
+- Get pinpointed answers to your queries
+- Utilize DuckDuckGo search engine for web searching
+- Use OpenAI GPT-4o for intelligent answer generation
+"""
