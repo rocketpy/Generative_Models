740bf6c54cba4d80ebd1504dfbae62cca95fa9fe
rocketpy <dimitr.ushakov@gmail.com>
2025-04-04 21:59:28 +0300

    Update AI_Search_Assistant.py

--- AI_Search_Assistant.py
+++ AI_Search_Assistant.py
@@ -9,3 +9,18 @@
 - Utilize DuckDuckGo search engine for web searching
 - Use OpenAI GPT-4o for intelligent answer generation
 """
+
+# Clone the GitHub repository
+"""
+git clone https://github.com/Shubhamsaboo/awesome-llm-apps.git
+cd awesome-llm-apps/advanced_tools_frameworks/web_search_ai_assistant
+
+Install the required dependencies:
+pip install -r requirements.txt
+
+Get your OpenAI API Key
+Sign up for an OpenAI account (or the LLM provider of your choice) and obtain your API key.
+Run the Streamlit App
+
+streamlit run ai_websearch.py
+"""
