3d71c8f58c907a8590073a771fb46fec0b3ab063
rocketpy <dimitr.ushakov@gmail.com>
2025-05-21 23:38:21 +0300

    Create Mega_TTS_3.py

---
+++ Mega_TTS_3.py
@@ -0,0 +1,14 @@
+# MegaTTS3 - Official PyTorch Implementation
+
+
+# https://github.com/bytedance/MegaTTS3
+
+"""
+Key features:
+Lightweight and Efficient: The backbone of the TTS Diffusion Transformer has only 0.45B parameters.
+Ultra High-Quality Voice Cloning: You can try our model at Huggingface Demo. 
+The .wav and .npy files can be found at link1. 
+Submit a sample (.wav format, < 24s, and please do not contain space in filename) on link2 to receive .npy voice latents you can use locally.
+Bilingual Support: Supports both Chinese and English, and code-switching.
+Controllable: Supports accent intensity control  and fine-grained pronunciation/duration adjustment (coming soon).
+"""
