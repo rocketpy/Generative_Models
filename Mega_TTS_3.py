08e5e855932f0cc9466920f50d81caa9373d52a1
rocketpy <dimitr.ushakov@gmail.com>
2025-05-30 23:42:15 +0300

    Update Mega_TTS_3.py

--- Mega_TTS_3.py
+++ Mega_TTS_3.py
@@ -11,8 +11,20 @@ The .wav and .npy files can be found at link1.
 Submit a sample (.wav format, < 24s, and please do not contain space in filename) on link2 to receive .npy voice latents you can use locally.
 Bilingual Support: Supports both Chinese and English, and code-switching.
 Controllable: Supports accent intensity control  and fine-grained pronunciation/duration adjustment (coming soon).
-"""
+
 
 # Installation
-# Clone the repositoryn - git clone https://github.com/bytedance/MegaTTS3
-# cd MegaTTS3
+# Clone the repository 
+
+git clone https://github.com/bytedance/MegaTTS3
+
+cd MegaTTS3
+
+# Requirements (for Linux)
+
+# Create a python 3.10 conda env (you could also use virtualenv)
+conda create -n megatts3-env python=3.10
+conda activate megatts3-env
+pip install -r requirements.txt
+
+"""
