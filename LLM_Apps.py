eee69b00b7ec52fef8fcd990a330145b8ce73744
rocketpy <dimitr.ushakov@gmail.com>
2025-06-23 22:55:54 +0300

    Update LLM_Apps.py

--- LLM_Apps.py
+++ LLM_Apps.py
@@ -1,3 +1,21 @@
 # LLM Apps
 
 # https://github.com/Shubhamsaboo/awesome-llm-apps
+
+
+# Getting Started
+"""
+Clone the repository
+
+git clone https://github.com/Shubhamsaboo/awesome-llm-apps.git 
+
+Navigate to the desired project directory
+
+cd awesome-llm-apps/starter_ai_agents/ai_travel_agent
+
+Install the required dependencies
+
+pip install -r requirements.txt
+
+Follow the project-specific instructions in each project's README.md file to set up and run the app.
+"""
