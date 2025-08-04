f516028effd6c5d3483cffa8ada8a824f7c30897
rocketpy <dimitr.ushakov@gmail.com>
2025-08-04 23:08:33 +0300

    Create Gorilla_LLM.py

---
+++ Gorilla_LLM.py
@@ -0,0 +1,21 @@
+# Gorilla: Large Language Model Connected with Massive APIs
+
+
+# https://github.com/ShishirPatil/gorilla
+
+"""
+# About
+Gorilla enables LLMs to use tools by invoking APIs. Given a natural language query, 
+Gorilla comes up with the semantically- and syntactically- correct API to invoke.
+
+# Features
+With Gorilla, we are the first to demonstrate how to use LLMs to invoke 1,600+ 
+(and growing) API calls accurately while reducing hallucination. 
+This repository contains inference code for running Gorilla finetuned models, 
+evaluation code for reproducing results from our paper, and APIBench - the largest collection of APIs, 
+curated and easy to be trained on!
+
+Since our initial release, we've served ~500k requests and witnessed incredible adoption by developers worldwide. 
+The project has expanded to include tools, evaluations, leaderboard, 
+end-to-end finetuning recipes, infrastructure components, and the Gorilla API Store:
+"""
